f91c3c9a645f93acaab39cd0c2b056a3dc9e7e0d
Tejas R <tejasrajanna@gmail.com>
2021-05-28 11:31:09 +0530

    pin onclick functionality added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function Detail({info}) {
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      zindex: 100,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  
+
+function Detail({info,setVisible}) {
+    const classes = useStyles();
     return (
-        <div>
-            <p>{info.customerInfo}</p>
-        </div>
-    )
+        <Card className={classes.root}  variant="outlined">
+      <CardContent>
+      <CardActions>
+        <IconButton size="small"  style={{color:"red"}} 
+        onClick={()=>{setVisible(false)}} > 
+        <DeleteIcon /> </IconButton>
+      </CardActions>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {`TaskId=${info.taskId}`}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {`Name=${info.name}`}
+        </Typography>
+        <Typography className={classes.title} color="textSecondary">
+          {`Sequence=${info.seq}`}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {`Customerinfo=${info.customerInfo}`}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default Detail;
--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,13 +1,23 @@
 import { Icon } from '@iconify/react';
 import locationIcon from '@iconify/icons-mdi/map-marker';
-import React from 'react';
-//import Detail from './Detail';
+import React,{useState} from 'react';
+import Detail from './Detail';
 
-const Pin = ({ text, info }) => (
+const Pin = ({ text, info }) => {
+    
+    const [visible,setVisible] = useState(false);
+      return (
     <div className="pin">
-      <Icon icon={locationIcon} style={{color:"tomato"}} className="pin-icon" />
+      <Icon 
+      icon={locationIcon} style={{color:"tomato"}} 
+      onClick={()=>{setVisible(!visible)} }
+      className="pin-icon" />
       <p className="pin-text" style={{color:"papayawhip"}}><b>{text}</b></p>
+      {visible?
+      <Detail info={info} setVisible={setVisible} />:
+      null}
     </div>
   )
+      }
 
   export default Pin;
