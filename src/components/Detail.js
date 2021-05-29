b57039bde98f70cc8a73365fbfdeb7ec3a872528
Tejas R <tejasrajanna@gmail.com>
2021-05-29 22:11:30 +0530

    Updated card

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from '@material-ui/icons/Cancel';
+import CardHeader from '@material-ui/core/CardHeader';
 
 const useStyles = makeStyles({
     root: {
-      minWidth: 275,
-      zindex: 100,
-    },
+      minWidth: 200,
+  },
+
     bullet: {
       display: 'inline-block',
       margin: '0 2px',
       transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
     },
   });
   
@@ -29,24 +23,21 @@ const useStyles = makeStyles({
 function Detail({info,setVisible}) {
     const classes = useStyles();
     return (
-        <Card className={classes.root}  variant="outlined">
+        <Card className={classes.root} variant="outlined">
+          <CardHeader
+              action={
+                <IconButton
+                  style={{color:"red"}} 
+                  onClick={()=>{setVisible(false)}} > 
+                  <DeleteIcon/> 
+                </IconButton>
+              }
+              title={info.name?info.name:'Name missing'}
+              subheader={`TaskId=${info.taskId?info.taskId:'missing'}`}
+              />
       <CardContent>
-      <CardActions>
-        <IconButton size="small"  style={{color:"red"}} 
-        onClick={()=>{setVisible(false)}} > 
-        <DeleteIcon /> </IconButton>
-      </CardActions>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          {`TaskId=${info.taskId}`}
-        </Typography>
-        <Typography variant="h5" component="h2">
-          {`Name=${info.name}`}
-        </Typography>
-        <Typography className={classes.title} color="textSecondary">
-          {`Sequence=${info.seq}`}
-        </Typography>
-        <Typography variant="body2" component="p">
-          {`Customerinfo=${info.customerInfo}`}
+        <Typography variant="body2" color="textPrimary" >
+          {`Customerinfo=${info.customerInfo?info.customerInfo:'missing'}`}
         </Typography>
       </CardContent>
     </Card>
